7988cf2d77649b5df233cd9a8e27fffcde4654b0
cagataycavdar <82333762+cagataycavdar@users.noreply.github.com>
2021-04-24 16:06:47 +0300

    Xml kütüphanesi eklendi. Rss feed internetten çekilip listbox'a yazdırıldı.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,6 +46,7 @@ namespace VizeOdevi
             // 
             this.timer1.Enabled = true;
             this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace VizeOdevi
 {
@@ -15,6 +17,24 @@ namespace VizeOdevi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string rss = ("http://koeri.boun.edu.tr/rss/");
+            XmlDocument deprem = new XmlDocument();
+            deprem.Load(rss);
+
+            var nodes = deprem.SelectNodes("rss/channel/item");
+
+            foreach (XmlNode node in nodes)
+            {
+                string title = node["title"].InnerText;
+                string date = node["pubDate"].InnerText;
+
+                listBox1.Items.Add(title + "  ---  " + date);
+            }
+
         }
     }
 }
