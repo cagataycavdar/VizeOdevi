b995355c83aecb909a4dfd621540205a7ae7a513
cagataycavdar <82333762+cagataycavdar@users.noreply.github.com>
2021-04-24 15:56:58 +0300

    Form tasarımı yapıldı. Timer eklendi. 5sn de bir kendini güncelleyecek şekilde ayarlandı ve aktif edildi.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,12 +30,39 @@ namespace VizeOdevi
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(703, 433);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(727, 450);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
